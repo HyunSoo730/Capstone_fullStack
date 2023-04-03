3a17bfcc5ab3a5106472f1ea49c88b3cd04bb7d2
pip-izony <eeodqql09@gmail.com>
2023-04-03 11:18:14 +0900

    Feat: Kakao_Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import SignUp from './SignUp'
-
+import logo from "./logo.svg";
+import "./App.css";
+import Auth from "./Auth";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
+  const REST_API_KEY = "8d2b99868ab67054454a535a7db9fc4f";
+  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   return (
+    <Router>
     <div className="App">
-      <SignUp />
+      <Routes>
+        <Route exact path="/" element={<h1><a href={KAKAO_AUTH_URL}>Kakao Login</a></h1>}/>
+        <Route path="/oauth/kakao/callback" element={<Auth />}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <App />
   </React.StrictMode>
 );
