0d713bdb802a6379b7e373f4f354e4241af7da3d
HyunSoo730 <whgustn730@gmail.com>
2023-04-13 20:47:25 +0900

    fix: file update

--- fullStack/src/main/java/capstone/fullstack/dto/MemberDto.java
+++ fullStack/src/main/java/capstone/fullstack/dto/MemberDto.java
@@ -1,12 +1,12 @@
-package capstone.fullstack.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class MemberDto {
-    private String name;
-    private String email;
-    private String password;
-}
+//package capstone.fullstack.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class MemberDto {
+//    private String name;
+//    private String email;
+//    private String password;
+//}
--- fullStack/src/test/java/capstone/fullstack/service/MemberServiceTest.java
+++ fullStack/src/test/java/capstone/fullstack/service/MemberServiceTest.java
@@ -1,63 +1,63 @@
-package capstone.fullstack.service;
-
-import capstone.fullstack.domain.Member;
-import capstone.fullstack.repository.MemberRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@Transactional
-@Slf4j
-class MemberServiceTest {
-
-    @Autowired
-    MemberRepository memberRepository;
-    @Autowired
-    MemberService memberService;
-
-    @Test
-    void 회원가입() {
-        //given
-        Member member = new Member();
-        member.setEmail("hyunsoo730@naver.com");
-        //when
-        Member saveMember = memberService.join(member);
-        //then
-        Assertions.assertThat(saveMember).isEqualTo(memberRepository.findByEmail(member.getEmail()));
-
-    }
-
-
-    @Test
-    void 중복_회원_검증(){
-        //given
-        Member member = new Member();
-        member.setEmail("hyunsoo730@naver.com");
-
-        Member member2 = new Member();
-        member2.setEmail("hyunsoo730@naver.com");
-        //when
-        memberService.join(member);
-        memberService.join(member2);  //예외가 발생해야 한다.
-        //then
-        fail("예외가 발생해야 한다.");
-    }
-
-
-    @Test
-    void 로그인_검증(){
-        Member member = new Member();
-        member.setEmail("hyunsoo730@naver.com");
-        member.setPassword("asd");
-        memberService.join(member);
-        memberService.findMember(member);
-
-    }
-
-}
+//package capstone.fullstack.service;
+//
+//import capstone.fullstack.domain.Member;
+//import capstone.fullstack.repository.MemberRepository;
+//import lombok.extern.slf4j.Slf4j;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@Transactional
+//@Slf4j
+//class MemberServiceTest {
+//
+//    @Autowired
+//    MemberRepository memberRepository;
+//    @Autowired
+//    MemberService memberService;
+//
+//    @Test
+//    void 회원가입() {
+//        //given
+//        Member member = new Member();
+//        member.setEmail("hyunsoo730@naver.com");
+//        //when
+//        Member saveMember = memberService.join(member);
+//        //then
+//        Assertions.assertThat(saveMember).isEqualTo(memberRepository.findByEmail(member.getEmail()));
+//
+//    }
+//
+//
+//    @Test
+//    void 중복_회원_검증(){
+//        //given
+//        Member member = new Member();
+//        member.setEmail("hyunsoo730@naver.com");
+//
+//        Member member2 = new Member();
+//        member2.setEmail("hyunsoo730@naver.com");
+//        //when
+//        memberService.join(member);
+//        memberService.join(member2);  //예외가 발생해야 한다.
+//        //then
+//        fail("예외가 발생해야 한다.");
+//    }
+//
+//
+//    @Test
+//    void 로그인_검증(){
+//        Member member = new Member();
+//        member.setEmail("hyunsoo730@naver.com");
+//        member.setPassword("asd");
+//        memberService.join(member);
+//        memberService.findMember(member);
+//
+//    }
+//
+//}
