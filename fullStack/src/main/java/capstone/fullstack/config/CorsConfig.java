b37bb9fa1b29b9e8e1527429097529b57ce743a6
HyunSoo730 <whgustn730@gmail.com>
2023-04-14 15:35:19 +0900

    git ignore 다시 적용

---
+++ fullStack/src/main/java/capstone/fullstack/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package capstone.fullstack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*"); //(1)
+        config.addExposedHeader("*"); //(2)
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
