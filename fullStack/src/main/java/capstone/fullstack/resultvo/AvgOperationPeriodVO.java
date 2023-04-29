802be45e8d0d7af7b6f3deaf50f4a931b55683e8
JH <es1564@naver.com>
2023-04-29 20:55:52 +0900

    merge 해결

---
+++ fullStack/src/main/java/capstone/fullstack/resultvo/AvgOperationPeriodVO.java
@@ -0,0 +1,10 @@
+package capstone.fullstack.resultvo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AvgOperationPeriodVO {
+    private Float avg_period;
+}
