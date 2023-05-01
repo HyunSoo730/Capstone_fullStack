4d597f93ef5bcf1de8570db334c9893d6dfaef01
pip-izony <eeodqql09@gmail.com>
2023-05-02 03:30:39 +0900

    test: github merge

--- fullStack/src/main/java/capstone/fullstack/repository/youtube/VideoCountRepository.java
+++
@@ -1,7 +0,0 @@
-package capstone.fullstack.repository.youtube;
-
-import capstone.fullstack.domain.youtube.VideoCount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoCountRepository extends JpaRepository<VideoCount, String> {
-}
