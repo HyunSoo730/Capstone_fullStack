85a0cb3d15d1b8b4baed69ac9b4a7c44812f9cac
pip-izony <eeodqql09@gmail.com>
2023-05-19 08:04:19 +0900

    feat: mouseover color END

--- src/Auth.js
+++ src/Auth.js
@@ -10,33 +10,12 @@ const Auth = () => {
     const AUTH_CODE = params.get('code');
 
     useEffect(() => {
-        fetch(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=http://localhost:3000/oauth/kakao/callback&code=${AUTH_CODE}`, {
+      console.log(`http://3.39.41.194:8080/auth/kakao/callback?code=${AUTH_CODE}`)
+        fetch(`/auth/kakao/callback?code=${AUTH_CODE}`, {
           method: 'GET',
           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         })
-          .then(res => res.json())
-          .then(data => {
-            if (data.access_token) {
-              console.log(data.access_token);
-              fetch('/auth/kakao/token', {
-                method: 'POST',
-                headers: {
-                  Authorization: data.access_token,
-                  'Content-Type': 'application/x-www-form-urlencoded'
-                },
-              })
-                .then(response => response.json())
-                .then(result => {
-                  if (result.access_token) {
-                    alert('WELCOME');
-                    //navigate('/');
-                  } else {
-                    alert('NONONO');
-                    //navigate('/');
-                  }
-                });
-            }
-          });
+        .then(res => {console.log(res)})
       }, []);
     return (
         <div>
