da9bd04505943e10418ccf4736c34c3904553fc7
HyunSoo730 <whgustn730@gmail.com>
2023-05-19 17:57:36 +0900

    Modified: Delete garbage file

--- fullStack/src/test/java/capstone/fullstack/FullstackApplicationTests.java
+++ fullStack/src/test/java/capstone/fullstack/FullstackApplicationTests.java
@@ -1,13 +1,13 @@
-package capstone.fullstack;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FullstackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package capstone.fullstack;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FullstackApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
